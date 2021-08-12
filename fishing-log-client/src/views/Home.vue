6a9a409212766644f43a6e1d7cb74f11b4c49ac4
Bennett Anderson <bennett.b.anderson@gmail.com>
2021-08-12 12:21:16 -0400

    importing vuetify

--- fishing-log-client/src/views/Home.vue
+++ fishing-log-client/src/views/Home.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
-    <h1>Welcome back, {{$store.state.user.username}}</h1>
-    <router-link v-bind:to="{ name: 'trips' }">View Trips</router-link> | 
-    <router-link v-bind:to="{ name: 'add-trip' }">Add a Trip</router-link>
-  </div>
+  <hello-world />
 </template>
 
 <script>
-export default {
-  name: "home"
-};
+  import HelloWorld from '../components/HelloWorld'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      HelloWorld,
+    },
+  }
 </script>
